0aada1fc97546f4bd2761472839c82e14d701b4a
Martinarcu <martin.arcu@hotmail.com>
2022-08-18 18:07:13 -0300

    carpetas creadas

---
+++ src/pages/Inicio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Inicio() {
+  return (
+    <div className='container-init'>
+        <h1 className='titulo-init'>Amazing Events</h1>
+    </div>
+  )
+}
