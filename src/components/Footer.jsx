86b67aa113d50e11e0e53c7e702a3de062845e71
Martinarcu <martin.arcu@hotmail.com>
2022-08-19 20:05:30 -0300

    le agregue las fotos de redes al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,12 @@ export default function Footer() {
     <div className='footer-container'>
       <p className='hover-1'>Richard Reyes - Martin Arcuri </p>
       <p className='hover-1'>Mindhub</p>
-    </div>
-    <div>
-      <a href=""><img src="" alt="" /></a>
+      <div className='redes'>
+        <a href="https://www.facebook.com/"><img src="/redes/facebook.svg" alt="" /></a>
+        <a href="#"><img src="/redes/instagram.svg" alt="" /></a>
+        <a href="#"><img src="/redes/linkedin.svg" alt="" /></a>
+        <a href="#"><img src="/redes/twitter.svg" alt="" /></a>
+      </div>
     </div>
     </>
   )
