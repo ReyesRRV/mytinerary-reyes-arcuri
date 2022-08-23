34c9fd8018d7534a68cc0ac957788fe6abfd3627
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-08-23 00:59:31 -0300

    todo funciona =)

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -2,55 +2,18 @@ import React from "react";
 import "../styles/Carusel.css";
 import Arrow from "./Arrow";
 import Arrow2 from "./Arrow2";
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 
-export default function Carrousel(props) {
-  const items = [
-    { url: "/img/Carousel/33-Uruguay.jpg", title: "Thirty-three - Uruguay" },
-    { url: "/img/Carousel/angels-USA.jpg", title: "The angels-USA" },
-    {
-      url: "/img/Carousel/blackriver-Argentina.jpg",
-      title: "Black River - Argentina",
-    },
-    {
-      url: "/img/Carousel/DaisyFlower-Venezuela.jpg",
-      title: "DaisyFlower island - Venezuela",
-    },
-    {
-      url: "/img/Carousel/goodAirs-Argentina.jpg",
-      title: "Good Airs - Argentina",
-    },
-    { url: "/img/Carousel/hotwater-mexico.jpg", title: "Hotwater - Mexico" },
-    { url: "/img/Carousel/jump-Argentina.jpg", title: "Jump - Argentina" },
-    {
-      url: "/img/Carousel/montevideo_grande.jpg",
-      title: "Herbvideo - Uruguay",
-    },
-    {
-      url: "/img/Carousel/portCross-Venezuela.jpg",
-      title: "Portcross - Venezuela",
-    },
-    { url: "/img/Carousel/stream-Argentina.jpg", title: "Stream - Argentina" },
-    { url: "/img/Carousel/thePeace-Mexico.jpg", title: "The Peace - Mexico" },
-    {
-      url: "/img/Carousel/TheSilver-Argentina.jpg",
-      title: "The Silver - Argentina",
-    },
-    {
-      url: "/img/Carousel/VineyardFromSea-Chile.jpg",
-      title: "Vineyard from Sea - Chile",
-    },
-  ];
+ function Carousel(props) {
+  let items = props.data;
+  let range = props.range;
+  let limitSlides = (props.slides * range);
+  let [start, setStart] = useState(0);
+  let [end, setEnd] = useState(start + range);
+  let [intervalId, setIntervalId] = useState();
+  let interval = props.interval * 1000;
 
-  const range = 4
-  const limitSlides = (props.slides * range)
-  const [start, setStart] = useState(0)
-  const [end, setEnd] = useState(start + range)
-  const [intervalId, setIntervalId] = useState()
-  const interval = props.interval * 1000
-
-  
-  const itemView = (item) => (
+  let itemView = (item) => (
     <div className="item">
       <p>{item.title}</p>
       <img src={item.url} />
@@ -58,28 +21,30 @@ export default function Carrousel(props) {
   );
   function previous() {
     if (start >= range) {
-        setStart(start-range)
-        setEnd(end-range)
+      setStart(start - range);
+      setEnd(end - range);
     } else {
-        setStart(limitSlides-range)
-        setEnd(limitSlides)
+      setStart(limitSlides - range);
+      setEnd(limitSlides);
     }
-}
+  }
 
-function next() {
-    if (start < limitSlides-range) {
-        setStart(start+range)
-        setEnd(end+range)
+  function next() {
+    if (start < limitSlides - range) {
+      setStart(start + range);
+      setEnd(end + range);
     } else {
-        setStart(0)
-        setEnd(range)
+      setStart(0);
+      setEnd(range);
     }
-}
+  }
   return (
     <div className="carousel">
-        <Arrow icon={"<"} click={previous}/>
-      <div className="slide">{items.slice(start, range).map(itemView)}</div>
-        <Arrow2 icon={">"} click={next}/>
+      <Arrow icon={"<"} click={previous} />
+      <div className="slide">{items.slice(start, end).map(itemView)}</div>
+      <Arrow2 icon={">"} click={next} />
     </div>
   );
 }
+
+export default Carousel
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Carrousel from '../components/Carrousel';
+import EventCarousel from '../components/Places'
 
 export default function Home() {
   return (
     <div> 
-      <Carrousel />
+      <EventCarousel />
+      {/* <Carrousel /> */}
     </div>
   )
 }
