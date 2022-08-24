9816f5f0d3db5e543f65202b46d293ec0ce4931c
Martinarcu <martin.arcu@hotmail.com>
2022-08-24 18:10:42 -0300

    1

---
+++ src/layouts/UnderConstriction.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UnderConstriction() {
+  return (
+    <div>UnderConstriction</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div> 
       <EventCarousel />
-      {/* <Carrousel /> */}
+      <Carrousel />
     </div>
   )
 }
