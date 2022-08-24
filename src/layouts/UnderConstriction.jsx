2765591a8750585d483244e3f07d941ef591ef4d
Martinarcu <martin.arcu@hotmail.com>
2022-08-24 19:01:02 -0300

    undercontruction

--- src/layouts/UnderConstriction.jsx
+++ src/layouts/UnderConstriction.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import CallToAction from '../components/Hero'
+import HomesiteLayout from './HomesiteLayout'
 
 export default function UnderConstriction() {
   return (
-    <div>UnderConstriction</div>
+    <HomesiteLayout>
+      <CallToAction buttonText='404'/>
+      <h4>Not fund or under contruction`s page!</h4>
+      <a href="/#"  >Back</a>
+    </HomesiteLayout>
   )
 }
