74db2a8cb3fd8a60573e88fb63b3de87c4490cb9
Martinarcu <martin.arcu@hotmail.com>
2022-08-24 16:33:15 -0300

    cambios minimos en el app, para q funcione el ruteo

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import './styles/Welcome.css';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Cities from './pages/Cities';
-import Places from './components/Places'
+import {BrowserRouter , Routes, Route, } from 'react-router-dom'
 
 function App() {
   return (
     <>
-
       <BrowserRouter>
         <Routes>
           <Route path="/Welcome" element={<Welcome />} />
+          <Route path="/Cities" element={<Cities />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
-      <Footer />
+
+      {/* <Footer /> */}
 
     </>
   );
