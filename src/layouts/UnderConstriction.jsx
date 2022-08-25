818dbe6d55ad184288677c4309f90d8a10f2047f
Martinarcu <martin.arcu@hotmail.com>
2022-08-24 23:36:06 -0300

    123

--- src/layouts/UnderConstriction.jsx
+++ src/layouts/UnderConstriction.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import CallToAction from '../components/Hero'
 import '../styles/UnderConstruction.css'
+import Header from '../components/Header'
+import Footer2 from '../components/Footer2'
 
 export default function UnderConstriction() {
   let buttonText= "Back"
   return (
     <>
+    <Header/>
       <div className='contenedorUnder'>
         <h4>Not fund or under contruction`s page!</h4>
         <CallToAction linkTo='/' buttonText={buttonText}/>
       </div>
+      <Footer2/>
     </>
   )
 }
