45dcdcdfc8234bd357a5145a412d4afc7b9070c0
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-07 20:52:41 -0300

    Update Details.jsx

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import Footer2 from '../components/Footer2';
-import { Link as LinkRouter } from "react-router-dom"
+import Details from '../components/Details'
 
-export default function Details() {
+export default function detailsPage() {
   return (
-    <>
-    <div class="card">
-  <div class="card-info">
-    <p class="title">John Doe</p>
-    <p class="subtitle">Web Dev</p>
-  </div>
-  <div class="card-bio">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-</div>
-</>
+    <Details/>
   )
 }
