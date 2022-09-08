053c17563a590a6d91ba4b6f3ad3669036e18830
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-08 09:31:37 -0300

    cleaning code

--- src/components/InputDetails.jsx
+++ src/components/InputDetails.jsx
@@ -8,23 +8,7 @@ function InputDetails(props){
  
 
     const card =(item) =>(
-        //     <div className="CardCity-container">
-        //     <div className='CardCity-info'>
-        //         <div className='cardCity-close'>
-        //             <p className="CardCity-city">{item.city}</p>
-        //             <button className='close-button' onClick={back} >X</button>
-        //         </div>
-        //         <img src={item.photo} className="CardCity-img img-mobile" />
-        //         <p className="CardCity-country">{item.country}</p>
-        //         <div className='cardCity-info'>
-        //             <p className='cardCity-population'>Population: {item.population/*.toLocaleString("de-DE")*/}</p>
-        //             <p className='cardCity-fundation'>Fundation: {item.fundation/*.toLocaleString("de-DE")*/}</p>
-        //         </div>
-        //     </div>
-        // </div>
-
-
-        <div class="card CardCity-container">
+     <div class="card CardCity-container">
          <img src={item.photo} className="CardCity-img img-mobile" />
         <div class="card-info">
         <p className="text-title">{item.city} - {item.country}</p>
@@ -34,7 +18,7 @@ function InputDetails(props){
         <p className='cardCity-fundation'>Fundation: {new Date(item.fundation).getFullYear() } </p>
         <div class="card-footer">
         <div class="card-button">
-        <button className='close-button' onClick={() => navigate(-1)} >X</button>
+        <button className='close-button' onClick={() => navigate(-1)} >Go back papu 😎 </button>
         </div>
       </div></div>
 
