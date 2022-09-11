e550fb588527e63eeb2410362d2b2dba6cc8b576
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-11 00:47:23 -0300

    CCS for new and edit city

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,18 +7,19 @@ import { Link as LinkRouter } from "react-router-dom";
 
 export default function Form() {
   const items =[
-    {value: "City: ", type: "text", placeholder:" Berlin"},
-    {value: "County: ", type: "text",placeholder:" Germany"},
-    {value: "Photo: ", type: "text",placeholder:"Insert URL"},
-    {value: "Population: ",type: "number",placeholder:"Approximate Number"},
-    {value: "Fundation: ",type:"date",placeholder:"13/01/01"},
-    {value: "Details: ",type: "text",placeholder:"Description"},
+    {value: "City ", type: "text", placeholder:" Berlin"},
+    {value: "County ", type: "text",placeholder:" Germany"},
+    {value: "Photo ", type: "text",placeholder:"Insert URL"},
+    {value: "Population ",type: "number",placeholder:"Approximate Number"},
+    {value: "Details ",type: "text",placeholder:"Description"},
+    {value: "Fundation",type:"date",placeholder:"13/01/01" , id: "fundation"},
   ]
   
   return (
     <div className='containerFrom'>
       <h1>New City</h1>
       <form className='formNewCity' action="">
+
         <UseForm data={items} />
         <div className='sendEdit'>
         <button id='send' type='summit' className='buttonInp'>Send City</button>
--- src/components/UseForm.jsx
+++ src/components/UseForm.jsx
@@ -2,14 +2,18 @@ function UseForm(props) {
   const allInput = props.data;
 
   const FormItem = (item) => (
-    <label className="placeholder" htmlFor={item.value}>
+    <>
+      <div className="input-group" >
       <input
-        type={item.type} className="inputFloat"
+        type={item.type} className="input"
         required
         id={item.value}
-        placeholder={item.value}
-      />
-    </label>
+        placeholder=" "
+        />
+      <label className="placeholder" htmlFor={item.value} id={item.id} >
+      {item.value} </label>
+        </div>
+        </>
   );
   return <>
   {allInput.map(FormItem)}
